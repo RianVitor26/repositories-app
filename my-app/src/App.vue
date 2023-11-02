ebf694b8b1d5e3a4d4b08e86c6d29a5e126d6ce8
RianVitor26 <rianvitorlhe@gmail.com>
2023-11-02 19:41:15 -0300

    style: ajustando formulario

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <main class="w-full h-screen bg-gray-900">
-    <h1 class="text-gray-100 font-black text-xl pt-5 text-center">Seus repositórios</h1>
-    <div class="w-full h-full">
-      <div class="w-full flex justify-end px-5">
-        <Modal />
+    <div class="w-full h-full bg-gray-900 md:max-w-5xl mx-auto">
+      <h1 class="text-gray-100 font-black text-xl pt-5 text-center">Seus repositórios</h1>
+      <div class="w-full h-full">
+        <div class="w-full flex justify-end px-5">
+          <Modal />
+        </div>
+        <div className="w-full h-3/4 flex flex-col overflow-y-auto">
+          <Card title="Título do repositório"
+            description="Descrição do repositório: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis, consequuntur!"
+            language="Linguagem | framework"
+            color="cor" />
+        </div>
       </div>
-      <Card titulo="Título do repositório"
-        descricao="Descrição do repositório: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis, consequuntur!"
-        linguagem="Linguagem | framework" />
     </div>
   </main>
 </template>
@@ -23,3 +28,20 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.overflow-y-auto::-webkit-scrollbar {
+  width: 5px;
+} 
+.overflow-y-auto::-webkit-scrollbar-track {
+  background: rgb(31 41 55);
+}
+
+.overflow-y-auto::-webkit-scrollbar-thumb {
+  background: rgb(2 132 199);
+}
+
+.overflow-y-auto::-webkit-scrollbar-thumb:hover {
+  background: rgb(64, 180, 238);
+}
+</style>
--- my-app/src/components/Card.vue
+++ my-app/src/components/Card.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="w-11/12 mx-auto bg-gray-800 h-fit my-5 rounded-md p-5">
-      <h2 class="text-gray-200 font-bold">{{ titulo }}</h2>
-      <h3 class="text-gray-200 my-3">{{ descricao }}</h3>
+      <h2 class="text-gray-200 font-bold">{{ title }}</h2>
+      <h3 class="text-gray-200 my-3">{{ description }}</h3>
       <div class="w-full flex justify-between flex-wrap">
         <div class="flex gap-3 items-center">
           <div class="rounded-full w-4 h-4 bg-sky-500"></div>
-          <h4 class="text-gray-300">{{ linguagem }}</h4>
+          <h4 class="text-gray-300">{{ language }}</h4>
         </div>
       </div>
     </div>
@@ -14,10 +14,10 @@
   <script>
   export default {
     props: {
-      titulo: String,
-      descricao: String,
-      linguagem: String,
-      link: String
+      title: String,
+      description: String,
+      language: String,
+      color: String
     },
   };
   </script>
