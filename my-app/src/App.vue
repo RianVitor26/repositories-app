cf22dcfedc104bd6ba3f2cc59bbedc0621849251
RianVitor26 <rianvitorlhe@gmail.com>
2023-11-02 17:23:32 -0300

    style: criando componente card

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,11 +1,22 @@
-<script setup>
-
-</script>
-
 <template>
-    <h1 className="text-red-500">Hello world</h1>
-</template>
-
-<style scoped>
-
-</style>
+    <main class="w-full h-screen bg-gray-900">
+      <h1 class="text-gray-100 font-black text-xl pt-5 text-center">Seus repositórios</h1>
+      <div class="w-full h-full">
+        <div class="w-full flex justify-end px-5">
+          <button class="text-gray-100 bg-sky-700 py-3 px-5 rounded-md my-5 font-bold hover:bg-sky-600">Criar novo</button>
+        </div>
+        <Card titulo="Título do repositório" descricao="Descrição do repositório: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis, consequuntur!" linguagem="Linguagem | framework" />
+      </div>
+    </main>
+  </template>
+  
+  <script>
+  import Card from './components/Card.vue';
+  
+  export default {
+    components: {
+      Card,
+    },
+  };
+  </script>
+  
---
+++ my-app/src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="w-11/12 mx-auto bg-gray-800 h-52 my-5 rounded-md p-5">
+      <h2 class="text-gray-200 font-bold">{{ titulo }}</h2>
+      <h3 class="text-gray-200 mb-2">{{ descricao }}</h3>
+      <div class="w-full flex justify-between">
+        <div class="flex gap-3 items-center">
+          <div class="rounded-full w-4 h-4 bg-sky-500"></div>
+          <h4 class="text-gray-300">{{ linguagem }}</h4>
+        </div>
+        <a href={link}><button class="text-gray-100 bg-gray-700 py-2 px-4 rounded-md font-bold hover:bg-gray-600">Link</button></a>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      titulo: String,
+      descricao: String,
+      linguagem: String,
+      link: String
+    },
+  };
+  </script>
+  
