6b7ede24722bbaf0f67f3c5e294937a22640e5c8
RianVitor26 <rianvitorlhe@gmail.com>
2023-11-02 19:03:52 -0300

    style: criando modal e o form

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,22 +1,25 @@
 <template>
-    <main class="w-full h-screen bg-gray-900">
-      <h1 class="text-gray-100 font-black text-xl pt-5 text-center">Seus repositórios</h1>
-      <div class="w-full h-full">
-        <div class="w-full flex justify-end px-5">
-          <button class="text-gray-100 bg-sky-700 py-3 px-5 rounded-md my-5 font-bold hover:bg-sky-600">Criar novo</button>
-        </div>
-        <Card titulo="Título do repositório" descricao="Descrição do repositório: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis, consequuntur!" linguagem="Linguagem | framework" />
+  <main class="w-full h-screen bg-gray-900">
+    <h1 class="text-gray-100 font-black text-xl pt-5 text-center">Seus repositórios</h1>
+    <div class="w-full h-full">
+      <div class="w-full flex justify-end px-5">
+        <Modal />
       </div>
-    </main>
-  </template>
-  
-  <script>
-  import Card from './components/Card.vue';
-  
-  export default {
-    components: {
-      Card,
-    },
-  };
-  </script>
-  
+      <Card titulo="Título do repositório"
+        descricao="Descrição do repositório: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis, consequuntur!"
+        linguagem="Linguagem | framework" />
+    </div>
+  </main>
+</template>
+
+<script>
+import Card from './components/Card.vue';
+import Modal from './components/Modal.vue';
+
+export default {
+  components: {
+    Card,
+    Modal,
+  },
+};
+</script>
---
+++ my-app/src/components/Modal.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-button className="text-gray-100 px-3 py-2 my-3 rounded-md bg-sky-600" text @click="dialogVisible = true">
+    Criar novo
+  </el-button>
+
+  <el-dialog className="bg-gray-900 absolute z-40 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-5 w-11/12"
+    v-model="dialogVisible" draggable>
+    <form className="flex flex-col">
+      <label for="title" className="pb-1 font-bold text-gray-100">Título</label>
+      <input className="mb-3 p-2 bg-gray-800 rounded-md text-gray-100" type="text">
+      <label for="description" className="pb-1 font-bold text-gray-100">Descrição</label>
+      <textarea name="description" id="description" rows="5" cols="50"
+        className="p-2 bg-gray-800 rounded-md text-gray-100"></textarea>
+      <select className="my-5 p-2 bg-gray-800 rounded-md text-gray-100" name="languages" id="languages">
+        <option v-for="language in languages" :key="language" :value="language">{{ language }}</option>
+      </select>
+      <label className="pb-1 font-bold text-gray-100" for="color">Cor</label>
+      <div className="flex w-full items-center justify-around">
+        <input className="p-2 bg-gray-800 rounded-md text-gray-100" type="color" v-model="selectedColor">
+        <div :style="{ backgroundColor: selectedColor }" className="w-5 h-5 rounded-full"></div>
+      </div>
+    </form>
+  </el-dialog>
+</template>
+  
+<script lang="js" setup>
+import { ref } from 'vue'
+
+const dialogVisible = ref(false)
+const selectedColor = ref('#000000')
+
+const languages = ['TypeScript', 'JavaScript', 'Java', 'C#', 'C', 'C++', 'Python', 'Php', 'Go', 'Ruby']
+</script>
