8589dc9fc36f46e2329290f61a43d007dfd6e33a
RianVitor26 <rianvitorlhe@gmail.com>
2023-11-05 04:36:33 -0300

    refac: removendo logica para editar e começar do 0

--- my-app/src/components/Modal.vue
+++ my-app/src/components/Modal.vue
@@ -4,8 +4,7 @@
     Criar novo
   </el-button>
 
-  <el-dialog class="bg-gray-900 p-5 md:w-3/6 w-11/12"
-    v-model="dialogVisible" draggable>
+  <el-dialog class="bg-gray-900 p-5 md:w-3/6 w-11/12" v-model="dialogVisible" draggable>
     <form @submit.prevent="dialogMode === 'create' ? createRepo() : updateRepo()" class="flex flex-col">
       <label for="title" class="pb-1 font-bold text-gray-100">Título</label>
       <input v-if="dialogMode === 'create'" v-model="newTitle" minlength="1" maxlength="50"
@@ -21,8 +20,8 @@
         placeholder="Insira a descrição do repo" required name="description" id="description" rows="5" cols="50"
         class="p-2 bg-gray-800 rounded-md text-gray-100"></textarea>
       <label for="language" class="pb-1 pt-3 font-bold text-gray-100">Linguagem</label>
-      <select v-if="dialogMode === 'create'" v-model="newLang" required
-        class="p-2 bg-gray-800 rounded-md text-gray-100" name="languages" id="languages">
+      <select v-if="dialogMode === 'create'" v-model="newLang" required class="p-2 bg-gray-800 rounded-md text-gray-100"
+        name="languages" id="languages">
         <option v-for="language in languages" :key="language" :value="language">{{ language }}</option>
       </select>
       <select v-else v-model="editRepositoryDetails.language" required
@@ -32,8 +31,7 @@
 
       <div class="flex w-full items-center justify-start gap-x-3 my-5">
         <label class="pb-1 font-bold text-gray-100" for="color">Cor</label>
-        <input v-if="dialogMode === 'create'" v-model="newColor" required class="bg-gray-800 text-gray-100"
-          type="color">
+        <input v-if="dialogMode === 'create'" v-model="newColor" required class="bg-gray-800 text-gray-100" type="color">
         <input v-else v-model="editRepositoryDetails.color" required class="p-2 bg-gray-800 rounded-md text-gray-100"
           type="color">
         <div :style="{ backgroundColor: dialogMode === 'create' ? newColor : editRepositoryDetails.color }"
@@ -49,7 +47,7 @@
 import { ref, defineEmits } from 'vue';
 import db from '../model/database';
 
-const emits = defineEmits(['addRepository', 'deleteRepository']);
+const emits = defineEmits(['addRepository', 'deleteRepository', 'editRepository']);
 const dialogVisible = ref(false);
 const dialogMode = ref('create');
 const languages = ['TypeScript', 'JavaScript', 'Java', 'C#', 'C', 'C++', 'Python', 'Php', 'Go', 'Ruby'];
@@ -57,26 +55,54 @@ const newTitle = ref('');
 const newDesc = ref('');
 const newLang = ref('');
 const newColor = ref('#000000');
+const editRepositoryDetails = ref({});
 
 const createRepo = () => {
-  const novoRepositorio = {
+  const newRepo = {
     title: newTitle.value,
     description: newDesc.value,
     language: newLang.value,
     color: newColor.value,
   };
 
-  db.repositories.add(novoRepositorio).then(() => {
+  db.repositories.add(newRepo).then(() => {
     newTitle.value = '';
     newDesc.value = '';
     newLang.value = '';
     newColor.value = '#000000';
     dialogVisible.value = false;
 
-    emits('addRepository', novoRepositorio);
+    emits('addRepository', newRepo);
   }).catch((error) => {
     console.error('Erro ao adicionar repositório:', error);
   });
 };
+
+const updateRepo = () => {
+  const updatedRepository = {
+    id: editRepositoryDetails.id,
+    title: editRepositoryDetails.title,
+    description: editRepositoryDetails.description,
+    language: editRepositoryDetails.language,
+    color: editRepositoryDetails.color,
+  };
+
+  db.repositories.update(updatedRepository.id, updatedRepository).then(() => {
+
+    emits('editRepository', updatedRepository);
+  }).catch((error) => {
+    console.error('Erro ao atualizar repositório:', error);
+  });
+}
+
+const methods = {
+ openModal() {
+   dialogVisible.value = true;
+ },
+ setDialogMode(mode) {
+   dialogMode.value = mode;
+ },
+};
+
 
 </script>
