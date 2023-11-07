c6d2af81998bda396a14deb32b5e8f96a9da25d7
RianVitor26 <rianvitorlhe@gmail.com>
2023-11-06 22:06:28 -0300

    style: ajustando o card

--- my-app/src/components/Card.vue
+++ my-app/src/components/Card.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="w-11/12 mx-auto bg-gray-800 h-fit my-5 rounded-md p-5 relative shadow-md">
-    <div class="w-full h-2 absolute top-0 left-0 blur-sm" :style="{ backgroundColor: color }"></div> 
+  <div class="w-11/12 mx-auto bg-gray-800 h-fit my-5 rounded-md p-5 shadow-md">
     <h2 class="text-gray-200 font-bold break-words">{{ title }}</h2>
     <h3 class="text-gray-200 my-3 break-words">{{ description }}</h3>
     <div class="w-full flex justify-center gap-3 flex-wrap sm:justify-between">
       <div class="flex gap-3 items-center">
-        <div class="w-5 h-5 rounded-full blur-sm" :style="{ backgroundColor: color }"></div>
+        <div class="w-5 h-5 rounded-full" :style="{ backgroundColor: color }"></div>
         <h4 class="text-gray-300">{{ language }}</h4>
       </div>
       <div class="flex items-center gap-3">
